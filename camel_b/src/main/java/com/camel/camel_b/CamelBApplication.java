1f1c60d14173ce3573a201fa9ac1e88bfe8fa603
giraudev <giraudev@gmail.com>
2022-06-22 18:43:44 +0100

    initial configuration

---
+++ camel_b/src/main/java/com/camel/camel_b/CamelBApplication.java
@@ -0,0 +1,13 @@
+package com.camel.camel_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelBApplication.class, args);
+	}
+
+}
