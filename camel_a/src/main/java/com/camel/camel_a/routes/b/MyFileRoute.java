ef762b0429801c5fca80d0909bb652eb5a207f39
giraudev <giraudev@gmail.com>
2022-07-06 11:15:18 +0100

    creating queue

--- camel_a/src/main/java/com/camel/camel_a/routes/b/MyFileRoute.java
+++ camel_a/src/main/java/com/camel/camel_a/routes/b/MyFileRoute.java
@@ -3,7 +3,7 @@ package com.camel.camel_a.routes.b;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyFileRoute extends RouteBuilder{
 
 	@Override
