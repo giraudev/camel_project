09d82e1e1ec5df145951250cee34c7909e674d42
giraudev <giraudev@gmail.com>
2022-07-19 12:56:12 +0100

    kafka

---
+++ camel_b/src/main/java/com/camel/camel_b/KafkaReceiverRouter.java
@@ -0,0 +1,20 @@
+package com.camel.camel_b;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaReceiverRouter extends RouteBuilder{
+
+
+	@Override
+	public void configure() throws Exception {
+		from("kafka:myKafkaTopic")
+		.to("log:received-message-from-kafka");
+	}
+
+}
